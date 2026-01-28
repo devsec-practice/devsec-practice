e25e0cde6a105eba706f918d8d224b0edd395f88
Vijay-kumar-baikadi <vijayk.baikadi@gmail.com>
2026-01-28 09:15:31 +0530

    asking for parameters

--- 01Variable.sh
+++ 01Variable.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-firstvariable=$1
-lastvariable=$2
+#firstname=$1
+#lastname=$2
 
-echo $firstvariable $lastvariable
+echo "Enter First Name:"
+read firstname
+
+echo "Enter Last Name:"
+read lastname
+
+echo "Firstname is: $firstname and Lastname is: $lastname"
